f7a5adc06857eed8af5a80b119009b285cb1b14e
vorce <joel@vorce.se>
2013-03-08 19:46:03 +0100

    Refactor logorator into a class, Logorator

--- logorator.py
+++ logorator.py
@@ -4,8 +4,6 @@ creating generated graphic designs.
 
 2012 Joel Carlbark (c)
 """
-
-import random
 import sys
 
 import pyglet
@@ -17,50 +15,50 @@ import hud
 import world
 
 
-def init(width, height, inp=None):
-    pyglet.clock.set_fps_limit(60)
+class Logorator():
+    def __init__(self, width, height, inp=None):
+        pyglet.clock.set_fps_limit(60)
 
-    config = Config(sample_buffers=1, samples=4, depth_size=16,
-                    double_buffer=True,)
+        config = Config(sample_buffers=1, samples=4,
+                        depth_size=16, double_buffer=True,)
 
-    screen = pyglet.window.Window(width=width, height=height,
-                                  config=config)
+        self.window = pyglet.window.Window(width=width, height=height,
+                                           config=config)
 
-    w = world.World(screen, inp)
+        self.world = world.World(self.window, inp)
 
-    camera = utils.Camera(screen, zoom=100)
+        self.camera = utils.Camera(self.window, zoom=100)
 
-    h = hud.Hud(screen)
+        self.hud = hud.Hud(self.window)
 
-    e = eventhandler.Eventhandler(screen)
+        self.event_handler = eventhandler.Eventhandler(self.window)
 
-    return (screen, camera, h, w, e)
+    def start(self):
+        glClearColor(0, 0, 0, 1)
+        while not self.window.has_exit:
+            self.window.dispatch_events()
 
+            self.world.tick(self.event_handler)
 
-def main_loop(screen, camera, myhud, myworld, myeventhandler):
-    glClearColor(0, 0, 0, 1)
-    while not screen.has_exit:
-        screen.dispatch_events()
+            self.camera.world()
+            self.world.draw()
 
-        myworld.tick(myeventhandler)
+            self.camera.hud()
+            self.hud.draw()
 
-        camera.world()
-        myworld.draw()
-
-        camera.hud()
-        myhud.draw()
-
-        pyglet.clock.tick()
-        screen.flip()
+            pyglet.clock.tick()
+            self.window.flip()
 
 if __name__ == "__main__":
+    logorator = None
+
     if len(sys.argv) > 1:
         f = open(sys.argv[1], 'r')
         line = f.readline()
         inp = eval(line)  # hue, hue, hue
         if isinstance(inp, list):
-            (screen, cam, myhud, myworld, myeventhandler) = init(800, 800, inp)
+            logorator = Logorator(800, 800, inp)
     else:
-        (screen, cam, myhud, myworld, myeventhandler) = init(800, 800)
+        logorator = Logorator(800, 800)
 
-    main_loop(screen, cam, myhud, myworld, myeventhandler)
+    logorator.start()
