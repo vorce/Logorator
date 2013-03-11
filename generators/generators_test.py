37736e0fe53493b1d35545da02b5eead12b96c6f
vorce <joel@vorce.se>
2013-03-11 20:15:31 +0100

    Add more unit tests - For lsys framework - Started on module for tests for all generators

---
+++ generators/generators_test.py
@@ -0,0 +1,38 @@
+import unittest
+from hamcrest import *
+import polygen
+import lsysgen
+
+class PolygenTest(unittest.TestCase):
+    def test_polygen_triangle_should_have_4_point_pairs(self):
+        poly_gen = polygen.PolyGen()
+        triangle_sides = 3
+        
+        triangle_points = poly_gen.ngon(0, 0, 10, triangle_sides)
+        number_of_point_pairs = len(triangle_points) / 2
+
+        assert_that(number_of_point_pairs, is_(4))
+
+
+    def test_polygen_nonrotated_triangle_first_point_pair_should_be_next_to_origin(self):
+        poly_gen = polygen.PolyGen()
+        triangle_sides = 3
+        origin_x = 0.0
+        origin_y = 0.0
+        side_len = 10.0
+        
+        triangle_points = poly_gen.ngon(origin_x, origin_y,
+                                        side_len, triangle_sides)
+        first_point_pair = triangle_points[:2]
+        expected_point_pair = [origin_x + side_len, origin_y]
+        
+        assert_that(first_point_pair, is_(expected_point_pair))
+    
+    def test_polygen_rectangle_should_have_5_point_pairs(self):
+        poly_gen = polygen.PolyGen()
+        rectangle_sides = 4
+        
+        rectangle_points = poly_gen.ngon(0, 0, 10, rectangle_sides)
+        number_of_point_pairs = len(rectangle_points) / 2
+
+        assert_that(number_of_point_pairs, is_(5))
---
+++ generators/lsys_test.py
@@ -0,0 +1,41 @@
+import lsys
+import unittest
+from hamcrest import *
+
+class TestLSys(unittest.TestCase):
+    def test_produce_any_axiom_with_no_rules_should_give_the_axiom(self):
+        ls = lsys.LSys("", {}, 1)
+        my_axiom = "axiom"
+        
+        command = ls.produce(my_axiom, {})
+        
+        assert_that(command, is_(my_axiom))
+        
+
+    def test_produce_axiom_F_rules_FplusF_should_give_FplusF(self):
+        ls = lsys.LSys("", {}, 1)
+        my_axiom = "F"
+        my_rules = {"F": "F+F"}
+        
+        command = ls.produce(my_axiom, my_rules)
+        
+        assert_that(command, is_(my_rules[my_axiom]))
+
+
+    def test_produce_axiom_F_rules_FplusF_twice_should_give_FplusFplusFplusF(self):
+        ls = lsys.LSys("", {}, 1)
+        my_axiom = "F"
+        my_rules = {"F": "F+F"}
+        
+        command = ls.produce(ls.produce(my_axiom, my_rules), my_rules)
+        
+        assert_that(command, is_("F+F+F+F"))
+
+    def test_evolve_axiom_F_rules_FplusF_twice_should_give_same_as_produce_twice(self):
+        ls = lsys.LSys("", {}, 1)
+        my_axiom = "F"
+        my_rules = {"F": "F+F"}
+        
+        command = ls.evolve(2, my_axiom, my_rules)
+        
+        assert_that(command, is_(ls.produce(ls.produce(my_axiom, my_rules), my_rules)))
